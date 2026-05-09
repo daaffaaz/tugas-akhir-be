2bac929d013267108969cc57a8786d57b0957a6b
daaffaaz <daffaakmal2101@gmail.com>
2026-05-10 03:20:20 +0700

    fix: run migrate on every Vercel deploy

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,6 @@ pip install --break-system-packages -r requirements/base.txt
 
 python manage.py build_faiss_index --verbosity 1
 
+DJANGO_SETTINGS_MODULE=config.settings.production python manage.py migrate --no-input
+
 DJANGO_SETTINGS_MODULE=config.settings.production python manage.py collectstatic --no-input
